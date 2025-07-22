023ac80c496b3166e98add5dd1352cce43ff49eb
Jonathan <jonathan.kopp@t-online.de>
2025-07-23 00:03:27 +0200

    minimal working example

--- firmware/ESP32-Tracker/src/main.cpp
+++ firmware/ESP32-Tracker/src/main.cpp
@@ -1,19 +1,39 @@
-#include <Arduino.h>
-#include <stdio.h>
+#include <HardwareSerial.h>
+#include <TinyGPS++.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SECONDS 1000
+
+TinyGPSPlus gps;
+HardwareSerial GPS_Serial(2); // Use UART2 on ESP32
+
+const int RXPin = 23; // GPS TX -> ESP32 RX
+const int TXPin = 22; // GPS RX -> ESP32 TX
+const uint32_t GPSBaud = 9600;
+
+unsigned long lastPrintTime = 0;
+const unsigned long printInterval = 15 * SECONDS; // 15 seconds
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  printf("%d\n", result);
+  Serial.begin(115200);
+  GPS_Serial.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
+  Serial.println("GPS Reader Initialized");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  printf("test");
-}
+  while (GPS_Serial.available() > 0) {
+    gps.encode(GPS_Serial.read());
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) { return x + y; }
+  if (millis() - lastPrintTime > printInterval) {
+    lastPrintTime = millis();
+
+    if (gps.location.isValid()) {
+      Serial.print("Latitude: ");
+      Serial.println(gps.location.lat(), 6);
+      Serial.print("Longitude: ");
+      Serial.println(gps.location.lng(), 6);
+    } else {
+      Serial.println("Waiting for valid GPS data...");
+    }
+  }
+}
